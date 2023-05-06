b27fca7bc0bb7c5c7098e63a2a2f094b8325e347
Chase Redmon <redmon.chase@gmail.com>
2023-05-06 19:04:42 -0400

    Condense error handling error by creating a Throw helper class for exceptions

---
+++ src/CatBox.NET/Throw.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CatBox.NET;
+
+internal static class Throw
+{
+    public static void IfStringIsNullOrWhitespace(string? s, string exceptionMessage, [CallerArgumentExpression("s")] string memberName = "")
+    {
+        if (string.IsNullOrWhiteSpace(s))
+            throw new ArgumentNullException(memberName, exceptionMessage);
+    }
+
+    public static void IfNull(object? s, [CallerArgumentExpression("s")] string memberName = "")
+    {
+        if (s is null)
+            throw new ArgumentNullException(memberName, "Argument cannot be null");
+    }
+}
