f1ea172d2b5680f02415618f52433eb3fc925033
Chase Redmon <redmon.chase@gmail.com>
2023-07-01 10:42:44 -0400

    Const string

--- src/CatBox.NET/Enums/ApiAction.cs
+++ src/CatBox.NET/Enums/ApiAction.cs
@@ -4,47 +4,47 @@ using System.Diagnostics.CodeAnalysis;
 namespace CatBox.NET.Enums;
 
 /// <summary>
-/// A class for 
+/// A class for organizing actions that the library can take when talking to the API
 /// </summary>
 internal static class ApiAction
 {
     /// <summary>
     /// UploadFile => "fileupload"
     /// </summary>
-    public static string UploadFile => "fileupload";
-    
+    public const string UploadFile = "fileupload";
+
     /// <summary>
     /// UrlUpload => "urlupload"
     /// </summary>
-    public static string UrlUpload => "urlupload";
-    
+    public const string UrlUpload = "urlupload";
+
     /// <summary>
     /// DeleteFile => "deletefiles"
     /// </summary>
-    public static string DeleteFile => "deletefiles";
-    
+    public const string DeleteFile = "deletefiles";
+
     /// <summary>
     /// CreateAlbum => "createalbum"
     /// </summary>
-    public static string CreateAlbum => "createalbum";
-    
+    public const string CreateAlbum = "createalbum";
+
     /// <summary>
     /// EditAlbum => "editalbum"
     /// </summary>
-    public static string EditAlbum => "editalbum";
-    
+    public const string EditAlbum = "editalbum";
+
     /// <summary>
     /// AddToAlbum => "addtoalbum"
     /// </summary>
-    public static string AddToAlbum => "addtoalbum";
-    
+    public const string AddToAlbum = "addtoalbum";
+
     /// <summary>
     /// RemoveFromAlbum => "removefromalbum"
     /// </summary>
-    public static string RemoveFromAlbum => "removefromalbum";
-    
+    public const string RemoveFromAlbum = "removefromalbum";
+
     /// <summary>
     /// DeleteFromAlbum => "deletealbum"
     /// </summary>
-    public static string DeleteFromAlbum => "deletealbum";
+    public const string DeleteFromAlbum = "deletealbum";
 }
