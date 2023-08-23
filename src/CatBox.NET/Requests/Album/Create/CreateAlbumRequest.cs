8e251c15fe186c573b7a40d0b683247b7ee436ee
Chase Redmon <redmon.chase@gmail.com>
2023-08-22 22:36:13 -0400

    Remove CreateAlbum methods and consolidate request methods using AnyOf union type. Add AnyOf dependency. Add IAlbumUploadRequest.cs to abstract CreateAlbumRequest.cs and UploadToAlbumRequest.cs. Remove redundant upload methods in CatBox.cs. Rename Upload methods in CatBoxClient.cs. Change return type CatBoxClient.UplaodImage to IAsyncEnumerable<string?> instead of Task<string?>.

---
+++ src/CatBox.NET/Requests/Album/Create/CreateAlbumRequest.cs
@@ -0,0 +1,8 @@
+﻿using AnyOfTypes;
+
+namespace CatBox.NET.Requests.CatBox;
+
+public record CreateAlbumRequest : AlbumCreationRequest, IAlbumUploadRequest
+{
+    public required AnyOf<FileUploadRequest, IEnumerable<StreamUploadRequest>, UrlUploadRequest> UploadRequest { get; init; }
+}
